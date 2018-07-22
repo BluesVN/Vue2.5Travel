f1e3e9485c139bb11f7467f47752b63c16774041
Dylan <476618360@qq.com>
2018-07-23 00:49:13 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,90 @@
+<template>
+<div>
+  <div class="title">热销推荐</div>
+  <ul>
+    <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+      <img class="item-img" :src="item.imgUrl">
+      <div class="item-info">
+        <p class="item-title">{{item.title}}</p>
+        <p class="item-desc">{{item.desc}}</p>
+        <button class="item-button">查看详情</button>
+      </div>
+    </li>
+  </ul>
+</div>
+
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+          title: '上海迪士尼乐园',
+          desc: '浪漫之都大连，海洋主题乐园'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+          title: '上海迪士尼乐园',
+          desc: '浪漫之都大连，海洋主题乐园'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+          title: '上海迪士尼乐园',
+          desc: '浪漫之都大连，海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/mixins'
+
+.title
+  line-height: 0.8rem
+  margin-top: 0.2rem
+  background: #eee
+  text-indent: 0.2rem
+
+.item
+  display: flex
+  height: 1.9rem
+  overflow: hidden
+
+  .item-img
+    height: 1.7rem
+    width: 1.7rem
+    padding: 0.1rem
+
+  .item-info
+    flex: 1
+    padding: 0.1rem
+    min-width: 0
+
+    .item-title
+      line-height: 0.54rem
+      font-size: 0.32rem
+      ellipsis()
+
+    .item-desc
+      line-height: 0.4rem
+      color: #ccc
+      ellipsis()
+
+    .item-button
+      line-height: 0.44rem
+      margin-top: 0.16rem
+      background: #ff9300
+      padding: 0 0.2rem
+      border-radius: 0.06rem
+      color: #fff
+</style>
